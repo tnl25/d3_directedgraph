80826804d39c349f3559a2bdb66dc8ee4ba9731a
Trinh-Le <trinh.le@ibm.com>
2018-03-13 17:13:21 -0500

    switch data source, add label to nodes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,12 +22,21 @@ export class AppComponent implements OnInit {
     const color = d3.scaleOrdinal(d3.schemeCategory20);
 
     const simulation = d3.forceSimulation()
-      .force('link', d3.forceLink().id((d) => d['id']))
-      .force('charge', d3.forceManyBody())
+      .force('link', d3
+        .forceLink()
+        .id((d) => d['id'])
+        .strength((d) => d['strength'])
+      )
+      .force('charge', d3.forceManyBody().strength(-20))
       .force('center', d3.forceCenter(width / 2, height / 2));
 
-    d3.json('assets/miserables.json', (err, data) => {
+    d3.json('assets/animalkingdom.json', (err, data) => {
       if (err) { throw new Error('Bad data file!'); }
+      // console.log(data['nodes']);
+
+      function getNodeColor(node) {
+        return node.level === 1 ? 'red' : 'gray'
+      }
 
       const link = svg.append('g')
         .attr('class', 'links')
@@ -35,7 +44,8 @@ export class AppComponent implements OnInit {
         .data(data['links'])
         .enter()
         .append('line')
-        .attr('stroke-width', (d) => Math.sqrt(d['value']));
+        .attr('stroke-width', (d) => Math.sqrt(d['value']))
+        .attr('stroke', '#E5E5E5');
 
       const node = svg.append('g')
         .attr('class', 'nodes')
@@ -43,16 +53,27 @@ export class AppComponent implements OnInit {
         .data(data['nodes'])
         .enter()
         .append('circle')
-        .attr('r', 5)
-        .attr('fill', (d) => color(d['group']))
+        .attr('r', 10)
+        .attr('fill', getNodeColor)
         .call(d3.drag()
           .on('start', dragStarted)
           .on('drag', dragged)
           .on('end', dragEnded)
         );
 
-      node.append('title')
-        .text((d) => d['id']);
+      let textElements = svg.append('g')
+        .selectAll('text')
+        .data(data['nodes'])
+        .enter()
+        .append('text')
+        .text(function (d) {
+          let text = d['label']
+          console.log(text)
+          return text
+        })
+        .attr('font-size', 15)
+        .attr('dx', 15)
+        .attr('dy', 4);
 
       simulation
         .nodes(data['nodes'])
@@ -63,14 +84,18 @@ export class AppComponent implements OnInit {
 
       function ticked() {
         link
-          .attr('x1', function(d) { return d['source'].x; })
-          .attr('y1', function(d) { return d['source'].y; })
-          .attr('x2', function(d) { return d['target'].x; })
-          .attr('y2', function(d) { return d['target'].y; });
+          .attr('x1', function (d) { return d['source'].x; })
+          .attr('y1', function (d) { return d['source'].y; })
+          .attr('x2', function (d) { return d['target'].x; })
+          .attr('y2', function (d) { return d['target'].y; });
 
-      node
-        .attr('cx', function(d) { return d['x']; })
-        .attr('cy', function(d) { return d['y']; });
+        node
+          .attr('cx', function (d) { return d['x']; })
+          .attr('cy', function (d) { return d['y']; });
+
+        textElements
+          .attr('x', function (d) { return d['x'] })
+          .attr('y', function (d) { return d['y'] });
       }
     });
 
